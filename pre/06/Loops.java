f06dcd63d0051cfe0b8f6eec5cc6ff1f7abc7e5d
yuki feng <wjdnwdn@sjdn.com>
2021-09-30 08:19:17 -0400

    .

--- pre/06/Loops.java
+++ pre/06/Loops.java
@@ -1,5 +1,10 @@
 public class Loops {
-  public static void power (double x int n) {
-    System.out.print(n)
+  public static void power (x n) {
+    int i = x;
+    while x < 0 and x > 0 {
+
+    }
+    System.out.print(x)
+    System.out.print()
   }
 }
