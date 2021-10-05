ca8fb4523ec20e73bf6061ced27519866460dac1
yuki feng <wjdnwdn@sjdn.com>
2021-10-05 02:10:37 -0400

    .

---
+++ Greet.java
@@ -0,0 +1,17 @@
+/*
+Yuki Feng
+APCS
+HW 08 -- Refactor Freshie Zero
+2021 9 30
+*/
+
+public class Greet {
+	public static void main(String[] args) {
+		greet("Yukester");
+		greet("Kosta");
+		greet("Ariana");
+	}
+		public static void greet(String name) {
+			System.out.println("Why, hello there, " + name + ". How do you do?");
+	}
+}
