1914a66c0c3d3bf5a7152f4f521f41ce39d66b36
Yuki Feng <yukifeng@Yukis-MBP.fios-router.home>
2021-11-03 12:01:57 -0400

    update hw 28

--- cb/String-2/xyzThere.java
+++ cb/String-2/xyzThere.java
@@ -9,15 +9,15 @@ xyzThere("xyz.abc") → true
 
 public class xyzThere {
   public static boolean xyzThere(String str) {
-    int xyzCount = 0;
-    for (int i = 0; i <str.length()-4; i++) {
-      if (str.substring(i,i+4).equals(".xyz")) {
-        xyzCount = 0;
-      } else if (str.substring(i,i+3).equals("xyz")) {
-        xyzCount += 1;
+    if (str.length() >= 3 && str.substring(0, 3).equals("xyz")) {
+      return true;
+    }
+    for (int i = 0; i < str.length()-3; ++i) {
+      if ((str.charAt(i) != '.') && (str.substring(i+1, i+4).equals("xyz"))) {
+        return true;
       }
-    } return ((xyzCount)>0);
-  }
+    } return false;
+   }
 
   public static void main(String[] args) {
     System.out.println(xyzThere("abcxyz"));
