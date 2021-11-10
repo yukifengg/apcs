865cfbf87c2b05a1d8f4f9767fb8e60da013ca3b
Yuki Feng <yukifeng@yukimac.local>
2021-11-10 07:15:12 -0500

    slay

--- lab00_oink/DemoScanner.java
+++ lab00_oink/DemoScanner.java
@@ -5,23 +5,17 @@
    2021-11-09
    time spent: 2 hrs
    DEMO -- Use Scanner class to read from standard input
-
    USAGE:
    1. Read for understanding.
    2. Record predictions.
-
    3.
    $ javac <thisclass>.java
    $ java <thisclass>
    ...
-
    4. enter text, followed by ENTER to send
    ...
-
    5. Reconcile predictions with observations.
-
    ( CTRL-C to exit )
-
 **/
 
 import java.util.Scanner;  //Scanner is not part of java's std lib
@@ -31,7 +25,7 @@ public class DemoScanner {
   public static void main( String[] args ) {
 
     //instantiate a Scanner with STDIN as its bytestream
-    Scanner sc = new Scanner( words.in );
+    Scanner sc = new Scanner( System.in );
 
     while( sc.hasNext() ) {
       System.out.println( sc.next() );
