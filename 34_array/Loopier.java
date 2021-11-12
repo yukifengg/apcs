9162bf3956ad9fff9599fd7e590366c152f75ec7
yukifengg <yuki.2102005@gmail.com>
2021-11-12 16:46:18 -0500

    SLAY.

---
+++ 34_array/Loopier.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class Loopier {
+	randInts = new int[5];
+	public static int[] randArray(int size) {
+		Random random = new Random();
+		int[] a = new int[size];
+		for (int i=0; i<a.length(); i++) {
+
+		}
+	}
+	public static void populateArray() {
+		randInts = {Math.random()};
+	}
+
+	public static String intsToString() {
+		System.out.println(randInts[0]
+	}
+}
