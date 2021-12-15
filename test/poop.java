a7750ca8bd1d781a0772a04566a5e3d6b33e574f
Yuki Feng <yukifeng@yukimac.fios-router.home>
2021-12-15 07:22:37 -0500

    slay

---
+++ test/poop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Poop {
+
+  int health;
+  String color;
+
+  public void Poop() {
+    health = 100;
+    color = "Brown";
+  }
+
+  public void battle() {
+    Scanner
+  }
+  public static void main(String[] args) {
+    Poop g = new Poop();
+
+  }
+}
