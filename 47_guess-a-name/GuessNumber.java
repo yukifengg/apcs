2c4a75c427ff4749cebbf6154add1d2e611406d2
Yuki Feng <yukifeng@yukimac.fios-router.home>
2021-12-14 20:19:10 -0500

    Slay hw 47

--- 47_guess-a-name/GuessNumber.java
+++ 47_guess-a-name/GuessNumber.java
@@ -7,6 +7,7 @@ Time Spent: .5 hours
 
 /*
 DISCO:
+there is the chance of _lo and _hi becoming the same value so then we can end the guessing and revea; the number to user.
 QCC:
 */
 
@@ -32,14 +33,64 @@ public class GuessNumber {
   }
 
   public void playRec() {
-    System.out.print("Guess a number between " + _lo + "and " + _hi + ": ");
+    System.out.print("Guess a number between " + _lo + " and " + _hi + ": ");
     int guess = sc.nextInt();
+    if (_lo == _hi) {
+      System.out.print("The number was " + _target);
+      System.out.println("Number of tries: " + _guessCtr);
+    }
     if (guess == _target) {
-      System.out.println("YASSSSS! You got the correct number.")
+      System.out.println("YASSS!!! You got the correct number.");
+      System.out.println("Number of tries: " + _guessCtr);
+    }
+    else if (guess > _target) {
+      System.out.println("Too high!");
+      _hi = guess-1;
+      _guessCtr++;
+      playRec();
+    }
+    else if (guess < _target) {
+      System.out.println("Too low!");
+      _lo = guess+1;
+      _guessCtr++;
+      playRec();
     }
   }
 
-  public static void main(String[] args) {
+  public void playIter() {
+    int guess;
+    while (true){
+      System.out.print("Guess a number between " + _lo + " and " + _hi + ": ");
+      guess = sc.nextInt();
+      if (_lo == _hi) {
+        System.out.print("The number was " + _target);
+        System.out.println("Number of tries: " + _guessCtr);
+        break;
+      }
+      if (guess == _target) {
+        System.out.println("YASSS!!! You got the correct number.");
+        System.out.println("Number of tries: " + _guessCtr);
+        break;
+      }
+      else if (guess > _target) {
+        System.out.println("Too high!");
+        _hi = guess-1;
+      }
+      else if (guess < _target) {
+        System.out.println("Too low!");
+        _lo = guess+1;
+      }
+      _guessCtr++;
+    }
+  }
 
+  public void play() {
+    playRec();
+    //playIter();
+  }
+
+  public static void main(String[] args) {
+    GuessNumber g = new GuessNumber(1,100);
+    g.play();
   } //ends main
 } //ends class
