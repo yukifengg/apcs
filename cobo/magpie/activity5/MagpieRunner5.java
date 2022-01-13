495d3978cd6d422b2f5cfcfb4a9a0e612a261031
Yuki Feng <yuki.2102005@gmail.com>
2022-01-13 08:13:58 -0500

    cobo

--- cobo/magpie/activity5/MagpieRunner5.java
+++ cobo/magpie/activity5/MagpieRunner5.java
@@ -14,11 +14,11 @@ public class MagpieRunner5
 	public static void main(String[] args)
 	{
 		Magpie5 maggie = new Magpie5();
-		
+
 		System.out.println (maggie.getGreeting());
 		Scanner in = new Scanner (System.in);
 		String statement = in.nextLine();
-		
+
 		while (!statement.equals("Bye"))
 		{
 			System.out.println (maggie.getResponse(statement));
