de1636c2c5b4f006f74b6a8522613f9fa17e80ba
Yuki Feng <yuki.2102005@gmail.com>
2022-03-07 22:41:53 -0500

    hw71.......sigh

--- 71_unmask/Mysterion.java
+++ 71_unmask/Mysterion.java
@@ -1,73 +1,173 @@
-/*
-TNPG stingRAY: Rin Fukuoka, Abdullah Faruque, Yuki Feng
-APCS pd 08
-HW71: Whut Dis.
-2022-03-07
-time spent: .5 hrs
-*/
+//Team stingRAY (Rin Furuoka, Abdullah Faruque, Yuki Feng)
+//APCS pd8
+// HW71 -- Reading for intent, tracing for VICTORY
+//2022-03-07
+//time spent: 0.5 hrs
 
-public class Mysterion {
-  private int[] arr; //array
 
-  public String toString() {
-    String foo = "[";
-    for( int i = 0; i < arr.length; i++ ) {
-      foo += arr[i] + ",";
-    }
-    if ( foo.length() > 1 )
-      //shave off trailing comma
-      foo = foo.substring( 0, foo.length()-1 );
-    foo += "]";
-    return foo;
+/***
+ * SKEELTON for
+ * class Mysterion
+ * tests preliminary hypotheses re: utility of mystery algo
+ *
+ * <h1>It's a Bird, It's a Plane! It's.... MYSTERION</h1>
+ * The Mysterion method
+ * value a, b, and c are the first, last, and "middle" value respectively, but c can be anything.
+ * the method iterates through the array. if an element is less than the value at c, then it is sent to the beginning
+ * of the array, where it would in a position behind c.
+ * result: elements behind position c are less than the element at c. elements infront of position c are more than element at c.
+ * <p>
+ * <b>Note:</b> BLAH blah blah, yakkety schmakketty...
+ * @author  Trey Parker, Matt Stone
+ * @version 3.1415926535897932384626433
+ * @since   2022-03-07m
+ *
+ * algo as pseudocode:
+ * ------------------------------
+ *     v = arr[c]
+ *     swap arr[c], arr[b]
+ *     s = a
+ *     for i in [a..b-1]
+ *         if arr[i] < v
+ *             swap arr[s], arr[i]
+ *             s+=1
+ *     swap arr[b], arr[s]
+ *
+ * DISCO
+ * value a, b, and c are the first, last, and "middle" value respectively, but c can be anything.
+ * the method iterates through the array. if an element is less than the value at c, then it is sent to the beginning
+ * of the array, where it would in a position behind c.
+ * result: elements behind position c are less than the element at c. elements infront of position c are more than element at c.
+ *
+ * QCC - n/a
+ *
+ * q0: What does it do?
+ * a0:divides the array into two sections, the first section being less than the element at position c, the second
+ * position being greater than the element at position c.
+ *
+ * q1: O(?)
+ * a1: O(n) since the iterates through the the array and makes swaps when applicable.
+ *
+ ***/
+
+
+public class Mysterion
+{
+  //--------------v  HELPER METHODS  v--------------
+  //swap values at indices x, y in array o
+  public static void swap( int x, int y, int[] o )
+  {
+    int tmp = o[x];
+    o[x] = o[y];
+    o[y] = tmp;
   }
 
-  public Mysterion(int size) {
-    arr = new int[size];
-    for (int i = 0; i < arr.length; i++) {
-      arr[i] = (int) (Math.random() * 15); //fills array w values up to 15
+  //print input array
+  public static void printArr( int[] a )
+  {
+    for ( int o : a )
+      System.out.print( o + " " );
+    System.out.println();
+  }
+
+  //shuffle elements of input array
+  public static void shuffle( int[] d )
+  {
+    int tmp;
+    int swapPos;
+    for( int i = 0; i < d.length; i++ ) {
+      tmp = d[i];
+      swapPos = i + (int)( (d.length - i) * Math.random() );
+      swap( i, swapPos, d );
     }
   }
 
-  private void mysteryAlgo() {
-    int a = 0; //first pos in array
-    int b = arr.length - 1; //last pos in array
-    int c = b/2; //middle pos in array
-    int v = arr[c]; // sets v to value of middle pos array
+  //return int array of size s, with each element fr range [0,maxVal)
+  public static int[] buildArray( int s, int maxVal )
+  {
+    int[] retArr = new int[s];
+    for( int i = 0; i < retArr.length; i++ )
+      retArr[i] = (int)( maxVal * Math.random() );
+    return retArr;
+  }
+  //--------------^  HELPER METHODS  ^--------------
 
-    /**** swapping arr[c] and arr[b] ****/
-    int temp = arr[c];
-    arr[c] = arr[b];
-    arr[b] = temp;
-    /***********************************/
 
-    int s = a; //sets s to a
-    for (int i = 0; i <= b-1; i++) {
-      if (arr[i] < v) {
-        /**** swapping arr[s] and arr[i] ****/
-        temp = arr[s];
-        arr[s] = arr[i];
-        arr[i] = temp;
-        /***********************************/
-        s+=1;
-      }
+  /**
+   * int mysterion(int[],int,int,int)
+   * DESCRIP
+   *
+   * @param arr
+   * @param a
+   * @param b
+   * @param c
+   * @return int
+   *
+   */
+  public static int mysterion( int arr[], int a, int b, int c)
+  {
+	int v = arr[c];
+    swap(c, b, arr);
+    int s = a;
+    for(int i = a; i < b; i++){
+        if (arr[i] < v){
+            swap(s, i, arr);
+            s++;
+        }
     }
-
-    /**** swapping arr[b] and arr[s] ****/
-    temp = arr[b];
-    arr[b] = arr[s];
-    arr[s] = temp;
-    /***********************************/
-  } //end mysteryAlgo
+    swap(b, s, arr);
+    return 1;
+  }//end mysterion
 
 
-  public static void main(String[] args) {
-    Mysterion m = new Mysterion(5); //5 elem array
+  //main method for testing
+  public static void main( String[] args )
+  {
 
-    System.out.println("old array");
-    System.out.println(m);
-    m.mysteryAlgo();
 
-    System.out.println("new array");
-    System.out.println(m);
-  } //end main
-} //end class
+    //init test arrays of magic numbers
+    int[] arr1 = {8,21,17,69,343};
+    int[] arr3 = {1,28,33,4982,37};
+    int[] arr4 = {5,4,17,9000,6};
+    int[] arr5 = {3,0,16,599,1024};
+
+
+    // run mysterion on each array,
+    // holding a & b fixed, varying c...
+    for( int testC = 0; testC < 5; testC++ ) {
+    System.out.println("arr1: ");
+    printArr(arr1);
+    mysterion(arr1,0,4,testC);
+    System.out.println("after mysterion w/ a=0,b=4,c="
+    + testC +"...");
+    printArr(arr1);
+    System.out.println("-----------------------");
+
+    System.out.println("arr3:");
+    printArr(arr3);
+    mysterion(arr3,0,4,testC);
+    System.out.println("after mysterion w/ a=0,b=4,c="
+    + testC +"...");
+    printArr(arr3);
+    System.out.println("-----------------------");
+
+    System.out.println("arr4:");
+    printArr(arr4);
+    mysterion(arr4,0,4,testC);
+    System.out.println("after mysterion w/ a=0,b=4,c="
+    + testC +"...");
+    printArr(arr4);
+    System.out.println("-----------------------");
+
+    System.out.println("arr5:");
+    printArr(arr5);
+    mysterion(arr5,0,4,testC);
+    System.out.println("after mysterion w/ a=0,b=4,c="
+    + testC +"...");
+    printArr(arr5);
+    System.out.println("-----------------------");
+
+  }//end main
+
+}
+}//end class Mysterion
