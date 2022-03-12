8807f5621ae133eddce4ee93b22e608bae14d795
Yuki Feng <yuki.2102005@gmail.com>
2022-03-12 09:03:19 -0500

    cleaning

--- 45_suparr/ListInt.java
+++
@@ -1,8 +0,0 @@
-public interface ListInt{
-  int size ();
-  int get (int index);
-  int set (int index, int newVal);
-  boolean add (int newVal);
-  void add (int index, int num);
-  int remove (int index);
-}
