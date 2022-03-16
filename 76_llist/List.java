6ae84ab26e497c09498551c1b21cf95085ff3f27
Yuki Feng <yuki.2102005@gmail.com>
2022-03-15 20:34:08 -0400

    update hw76

--- 76_llist/List.java
+++ 76_llist/List.java
@@ -1,115 +1,24 @@
-// Team Acid: Yuki Feng, Tasnim Chowdhury, Faiyaz Rafee
-// APCS pd08
-// HW76: We Got a Little Ol' Convoy
-// 2022-03-14
-// time spent: 0.5 hrs
-
 /***
- * class LList
- * Implements a linked list of LLNodes, each containing String data
+ * interface List
+ * subset of actual Java List interface
+ * Specifies actions a List must be able to perform.
+ *
+ * If placed in current working dir, will be consulted instead of
+ *  built-in Java library interface.
  **/
 
-public class LList implements List //interface def must be in this dir
+public interface List
 {
+  //add node to list, containing input String as its data
+  public boolean add( String x );
 
-  //instance vars
-  private LLNode _head;
-  private int _size;
-  private List<String> list;
+  //return data in element at position i
+  public String get( int i );
 
-  // constructor -- initializes instance vars
-  public LList( )
-  {
-    // YOUR CODE HERE
-    list = new LinkedList<String>();
-    _head = new LLNode(null,null);
-    _size = 0;
-  }
+  //overwrite data in element at position i
+  public String set( int i, String x );
 
+  //return length of list
+  public int size();
 
-  //--------------v  List interface methods  v--------------
-
-  public boolean add( String newVal )
-  {
-    // YOUR CODE HERE
-    _head.setCargo(newVal);
-    size += 1;
-    return true;
-  }
-
-
-  public String get( int index )
-  {
-    if ( index < 0 || index >= size() )
-      throw new IndexOutOfBoundsException();
-    // YOUR CODE HERE
-    String stuff = _head.getCargo();
-    return stuff;
-  }
-
-
-  public String set( int index, String newVal )
-  {
-
-    if ( index < 0 || index >= size() )
-      throw new IndexOutOfBoundsException();
-
-    // YOUR CODE HERE
-    head.get(index)
-  }
-
-
-  //return number of nodes in list
-  public int size()
-  {
-    // YOUR CODE HERE
-    return _size;
-  }
-
-  //--------------^  List interface methods  ^--------------
-
-
-
-  // override inherited toString
-  public String toString()
-  {
-    // YOUR CODE HERE
-    return _cargo;
-  }
-
-
-  //main method for testing
-  public static void main( String[] args )
-  {
-    LList james = new LList();
-
-    System.out.println( james );
-    System.out.println( "size: " + james.size() );
-
-    james.add("beat");
-    System.out.println( james );
-    System.out.println( "size: " + james.size() );
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    james.add("a");
-    System.out.println( james );
-    System.out.println( "size: " + james.size() );
-
-    james.add("need");
-    System.out.println( james );
-    System.out.println( "size: " + james.size() );
-
-    james.add("I");
-    System.out.println( james );
-    System.out.println( "size: " + james.size() );
-
-    System.out.println( "2nd item is: " + james.get(1) );
-
-    james.set( 1, "got" );
-    System.out.println( "...and now 2nd item is: " + james.set(1,"got") );
-
-    System.out.println( james );
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-  }
-
-}//end class LList
+}//end interface
