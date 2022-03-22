de2d8ca5f0bd0a0a476d800688033d65abe1416c
yukifengg <yuki.2102005@gmail.com>
2022-03-22 13:37:54 -0400

    hk

--- 78_dllist/List.java
+++ 78_dllist/List.java
@@ -7,11 +7,11 @@
  * If placed in current working dir, will be consulted instead of
  *  built-in Java library interface.
  *
- * new in version 2.0: 
+ * new in version 2.0:
  * REMOVE-AT-INDEX
  * ADD-AT-INDEX
 =======
- * 
+ *
  * If placed in current working dir, will be consulted instead of
  *  built-in Java library interface.
 >>>>>>> 4f6b1f3dc40fadfcbc683ef4663648020391beb4
@@ -21,13 +21,8 @@ public interface List
 {
   //add node to list, containing input String as its data
   public boolean add( String x );
-
-<<<<<<< HEAD
   //insert at specified index
   public void add( int index, String newVal );
-
-=======
->>>>>>> 4f6b1f3dc40fadfcbc683ef4663648020391beb4
   //remove node from list, return its data
   public String remove( int index );
 
@@ -39,9 +34,4 @@ public interface List
 
   //return length of list
   public int size();
-
-<<<<<<< HEAD
 }//end interface
-=======
-}//end interface
->>>>>>> 4f6b1f3dc40fadfcbc683ef4663648020391beb4
