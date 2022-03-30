9733035a7af456e37fb2ca6953536ebdef949a32
Yuki Feng <yuki.2102005@gmail.com>
2022-03-29 20:19:39 -0400

    hw83

--- 83_stack/Latkes.java
+++ 83_stack/Latkes.java
@@ -1,25 +1,8 @@
-// Team Acid: Yuki Feng, Tasnim Chowdhury, Faiyaz Rafee
-// APCS pd08
-// HW83: Stacks on Stacks
-// 2022-03-28
-// time spent: 0.5 hrs
-
 /***
  * class Latkes
  * v1
- * SKELETON
  * Implements a stack of Strings using an encapsulated array
  **/
-
-/***
-    DISCO
-    - To remove the outermost element, you just change the size of the stack
-    - Must increase size after pushing element onto the current stack position
-    - Size is used like a position tracker
-    QCC
-    - What are some instances where stacks would be more beneficial to use than linked lists?
- **/
-
 
 public class Latkes
 {
@@ -32,50 +15,53 @@ public class Latkes
   {
     _stack = new String[initCapacity];
     _stackSize = 0;
-  }// O(?)
+  }
 
 
   //means of insertion
   public void push( String s )
   {
-    if (isFull()) {
-      return;
+    //if necessary, create more storage space
+    if ( isFull() ) {
+      String[] temp = new String[ _stack.length * 2 ];
+      for( int i = 0; i < _stack.length; i++ ) {
+        temp[i] = _stack[i];
+      }
+      _stack = temp; //old _stack freed by JGC
     }
+    //add new element at end of array
+    //Q: Why is this more efficient than inserting at front?
     _stack[_stackSize] = s;
     _stackSize++;
-    System.out.println("pushed " + s );
-  }// O(1)
+  }
 
 
   //means of removal
-  public String pop( )
+  public String pop()
   {
-    if (isEmpty()) {
-      return null;
-    }
-      _stackSize--;
-      return _stack[_stackSize];
-  }// O(1)
+    String retStr = "";
+    if ( isEmpty() )  return null;
+    retStr = _stack[ _stackSize---1 ];
+    return retStr;
+  }
 
 
   //chk for emptiness
   public boolean isEmpty()
   {
-    return _stackSize < 1;
-  }// O(1)
-
+    return _stackSize == 0;
+  }
 
   //chk for fullness
   public boolean isFull()
   {
-    return _stack.length == _stackSize;
-  }// O(1)
+    return _stackSize >= _stack.length;
+  }
 
 
   //main method for testing
   public static void main( String[] args )
   {
-
     Latkes tastyStack = new Latkes(10);
 
     tastyStack.push("aoo");
@@ -118,6 +104,8 @@ public class Latkes
 
     //stack empty by now; SOP(null)
     System.out.println( tastyStack.pop() );
+    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+      ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
 
   }//end main()
 
