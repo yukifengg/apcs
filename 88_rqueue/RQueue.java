25c0f31a2cdf4332b0c86431fbe2bf9b2a3e3c5f
Yuki Feng <yuki.2102005@gmail.com>
2022-04-05 12:01:32 +0000

    hw88

---
+++ 88_rqueue/RQueue.java
@@ -0,0 +1,127 @@
+/***
+ * class RQueue
+ * SKELETON
+ * A node-based, randomized queue
+ * (a collection with FIIDKO property)
+ *
+ *       -------------------------------
+ *   end |  --->   Q U E U E   --->    | front
+ *       -------------------------------
+ *
+ *  linkages point opposite direction for O(1) en/dequeuing
+ *            N <- N <- ... <- N <- N
+ *      _end -^                     ^- _front
+ *
+ **/
+
+
+public class RQueue<SWASHBUCKLE> implements Queue<SWASHBUCKLE>
+{
+  //instance variables
+  private LLNode<SWASHBUCKLE> _front, _end;
+  private int _size;
+
+
+  // default constructor creates an empty queue
+  public RQueue()
+  {
+    _size = 0;
+    _front = null;
+    _end = null;
+  }
+
+
+  public void enqueue( SWASHBUCKLE enQVal )
+  {
+    //special case: when enqueuing to an empty list,
+    //make _front && _end point to same node
+    if ( isEmpty() ) {
+      _front = _end = new LLNode<SWASHBUCKLE>( enQVal, null );
+    }
+    else {
+      _end.setNext( new LLNode<SWASHBUCKLE>( enQVal, null ) );
+      _end = _end.getNext();
+    }
+    _size++;
+    System.out.println("enqueued " + enQVal);
+  }//O(1)
+
+
+  // remove and return thing at front of queue
+  // assume _queue ! empty
+  public T dequeue()
+  {
+    
+  }//O(1)
+
+
+  public T peekFront()
+  {
+    return _front.getCargo();
+  }//O(n)
+
+
+  /***
+   * void sample() -- a means of "shuffling" the queue
+   * Algo:
+   *   use random node in queue.
+   **/
+  public void sample ()
+  {
+    int ran = (int) (Math.random() * _size);
+
+
+
+  }//O(?)
+
+
+  public boolean isEmpty()
+  {
+    return _front == null;
+  } //O(1)
+
+
+  // print each node, separated by spaces
+  public String toString()
+  {
+    return;
+  }//end toString()
+
+
+
+  //main method for testing
+  public static void main( String[] args )
+  {
+
+      /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+
+    Queue<String> PirateQueue = new RQueue<String>();
+
+    System.out.println("\nnow enqueuing...");
+    PirateQueue.enqueue("Dread");
+    PirateQueue.enqueue("Pirate");
+    PirateQueue.enqueue("Roberts");
+    PirateQueue.enqueue("Blackbeard");
+    PirateQueue.enqueue("Peter");
+    PirateQueue.enqueue("Stuyvesant");
+
+    System.out.println("\nnow testing toString()...");
+    System.out.println( PirateQueue ); //for testing toString()...
+
+    System.out.println("\nnow dequeuing...");
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+
+    System.out.println("\nnow dequeuing fr empty queue...\n" +
+                       "(expect NPE)\n");
+    System.out.println( PirateQueue.dequeue() );
+
+      ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+
+  }//end main
+
+}//end class RQueue
