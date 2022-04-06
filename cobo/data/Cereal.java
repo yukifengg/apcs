b83bdd2bfbe9c70e4ade07db6d1ffe476523b416
Yuki Feng <yuki.2102005@gmail.com>
2022-04-06 12:50:03 -0400

    CEREAL

--- cobo/data/Cereal.java
+++ cobo/data/Cereal.java
@@ -1,47 +1,44 @@
+/*
+TEAM YAK: Yuki Feng, Anjini Katari, Kevin Li
+APCS pd 08
+L08: America's Next Top Data Scientist
+2022-04-05
+Time spent: 1.0 hours
+*/
 public class Cereal{
 
     //instance vars
     private int Calories, Protein, Sodium, Fiber, Fat, Carbs, Sugar, Potassium, Vits, Shelf;
-    private double weight, cups;
-    private float rating; 
+    private double weight, cups, rating;
     private char Type;
     private String name;
 
-    //constructor 
-    public Cereal(){
-        Calories = 0;
-        Potassium = 0;
-        Protein = 0; 
-        Sodium = 0;
-        Fiber = 0;
-        Fat = 0; 
-        Carbs = 0; 
-        Sugar = 0;
-        Vits = 0;
-        Shelf = 0
-        weight = 0.0;
-        cups = 0.0;
-        name = "";
-        Type = '';
-        rating = 0.00;
+    //constructor
+    public Cereal(int Calories, int Protein, int Sodium, int Fiber, int Fat,
+                  int Carbs, int Sugar, int Potassium, int Vits, int Shelf,
+                  double weight, double cups, double rating,
+                  char Type,
+                  String name){
+        this.Calories = Calories;
+        // this.Protein = Protein;
+        // this.Sodium = Sodium;
+        // this.Fiber = Fiber;
+        // this.Fat = Fat;
+        // this.Carbs = Carbs;
+        // this.Sugar = Sugar;
+        // this.Potassium = Potassium;
+        // this.Vits = Vits;
+        // this.Shelf = Shelf;
+        // this.weight = weight;
+        // this.cups = cups;
+        // this.rating = rating;
+        // this.Type = Type;
+        // this.name = name;
 
     }
 
     public String toString(){
-        
-    }    
+        yes;
+    }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
