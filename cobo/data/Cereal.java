5424a6c0ae5b253e0ad3ed9e35dac19b7de442c4
Yuki Feng <yuki.2102005@gmail.com>
2022-04-05 22:30:32 -0400

    data

---
+++ cobo/data/Cereal.java
@@ -0,0 +1,47 @@
+public class Cereal{
+
+    //instance vars
+    private int Calories, Protein, Sodium, Fiber, Fat, Carbs, Sugar, Potassium, Vits, Shelf;
+    private double weight, cups;
+    private float rating; 
+    private char Type;
+    private String name;
+
+    //constructor 
+    public Cereal(){
+        Calories = 0;
+        Potassium = 0;
+        Protein = 0; 
+        Sodium = 0;
+        Fiber = 0;
+        Fat = 0; 
+        Carbs = 0; 
+        Sugar = 0;
+        Vits = 0;
+        Shelf = 0
+        weight = 0.0;
+        cups = 0.0;
+        name = "";
+        Type = '';
+        rating = 0.00;
+
+    }
+
+    public String toString(){
+        
+    }    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
